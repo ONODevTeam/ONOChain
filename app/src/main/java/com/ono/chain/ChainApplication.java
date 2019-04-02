a96f69a10c6653275394939bf94b902a324df304
wangze <12345678>
2019-04-02 20:19:16 +0800

    编写一个可与性的demo

--- app/src/main/java/com/ono/chain/ChainApplication.java
+++ app/src/main/java/com/ono/chain/ChainApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChainApplication {
+public class  ChainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChainApplication.class, args);
--- app/src/main/java/com/ono/chain/abci/demo/DemoAbci.java
+++ app/src/main/java/com/ono/chain/abci/demo/DemoAbci.java
@@ -1,5 +1,10 @@
 package com.ono.chain.abci.demo;
 
+import com.ono.chain.jabci.socket.ExceptionListener.Event;
+import com.ono.chain.jabci.socket.TSocket;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
 /**
  * @project_name：ono-chain
  * @package_name：com.ono.chain.abci.demo
@@ -11,5 +16,35 @@ package com.ono.chain.abci.demo;
  * @remark   ***
  * @version V0.1
  */
-public class DemoAbci {
+@Component
+public class DemoAbci implements CommandLineRunner {
+    
+    private TSocket socket;
+    
+    @Override
+    public void run(String... args) throws Exception {
+        demoAbci();
+    }
+    
+    public void demoAbci() throws InterruptedException {
+        System.out.println("starting counter");
+        socket = new TSocket((socket, event, exception) -> {
+            if (event == Event.SocketHandler_handleRequest) {
+                exception.printStackTrace();
+            } else if (event == Event.SocketHandler_readFromStream) {
+                System.err.println("error on " + socket.orElse("NONAME") + "-> SocketHandler_readFromStream: " + exception.getMessage());
+            }
+        }, (socketName, count) -> {
+            System.out.println("CONNECT socketname: " + socketName + " count: " + count);
+        }, (socketName, count) -> {
+            System.out.println("DISCONNET socketname: " + socketName + " count: " + count);
+        });
+        
+        socket.registerListener(this);
+        
+        Thread t = new Thread(() -> socket.start(TSocket.DEFAULT_LISTEN_SOCKET_PORT));
+        t.setName("Java DemoAbci Main Thread");
+        t.start();
+    }
+    
 }
